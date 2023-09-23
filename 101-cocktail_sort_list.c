11055091cc460d38e10b2b764a0f7a3ca4c9bdad
mojtabababiker <mojmohammad98@gmail.com>
2023-09-24 02:34:13 +0300

    Add task 101

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -10,9 +10,12 @@ void swap_nodes(listint_t *node_1, listint_t *node_2, listint_t **list);
  */
 void cocktail_sort_list(listint_t **list)
 {
-	listint_t *node = *list, *temp = NULL;
+	listint_t *node = NULL, *temp = NULL;
 	bool swaped = true;
 
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+	
 	while (swaped)
 	{
 		swaped = false;
