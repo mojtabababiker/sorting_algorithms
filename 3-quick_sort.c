3d8b420694760efebb1a9c49c3be410a958972dd
asimsharf <asimsahrf@gmail.com>
2023-09-24 18:44:29 +0300

    Added github actions for insertion sort.

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -2,7 +2,7 @@
 
 /**
  * partitioning - partitioning the array according to aa randome
- *                selected number called pivot
+ *								selected number called pivot
  * @arr: the array to be partitioned
  * @lo: the start of the array
  * @hi: the end of the array
@@ -11,61 +11,61 @@
 
 int partitioning(int *arr, int lo, int hi)
 {
-  int hd= lo, tl = lo - 1;
-  int pivot = arr[hi], temp = 0;
+	int hd= lo, tl = lo - 1;
+	int pivot = arr[hi], temp = 0;
 
-  while (hd <= hi)
-    {
-      if (arr[hd] <= pivot)
+	while (hd <= hi)
+		{
+			if (arr[hd] <= pivot)
 	{
 
-	  /*swap the element in hd and t +1*/
-	  tl++;
-	  temp = arr[tl];
-	  arr[tl] = arr[hd];
-	  arr[hd] = temp;
+		/*swap the element in hd and t +1*/
+		tl++;
+		temp = arr[tl];
+		arr[tl] = arr[hd];
+		arr[hd] = temp;
 
 	}
-      hd++;
-    }
+			hd++;
+		}
 
-  return (tl);
+	return (tl);
 }
 
 
 /**
  * quickSort - recursive quick sort algrothim
  * @arr: the arr to be sorted
- * @hi: the  last index on the arr
+ * @hi: the	last index on the arr
  * @lo: the first index on the arr
- * @size: the  size of the array
+ * @size: the	size of the array
  */
 
 void quickSort(int *arr, int lo, int hi, size_t size)
 {
-  int pivot;
+	int pivot;
 
-  if (hi <= lo)
-    return;
-  
-  pivot = partitioning(arr, lo, hi);
-  print_array(arr, size);
-  quickSort(arr, lo, pivot - 1, size);
-  quickSort(arr, pivot + 1, hi, size);
+	if (hi <= lo)
+		return;
+	
+	pivot = partitioning(arr, lo, hi);
+	print_array(arr, size);
+	quickSort(arr, lo, pivot - 1, size);
+	quickSort(arr, pivot + 1, hi, size);
 
 }
 
 /**
  * quick_sort - sort array of integer in ascending order using
- *              Quick sort algrothim
+ *							Quick sort algrothim
  * @arr: array of integer that will be sorted
  * @size: the size of the array
  */
 
 void quick_sort(int *arr, size_t size)
 {
-  int lo = 0;
-  int hi = size - 1;
+	int lo = 0;
+	int hi = size - 1;
 
-  quickSort(arr, lo, hi, size);
+	quickSort(arr, lo, hi, size);
 }
